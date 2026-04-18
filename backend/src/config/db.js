803610f6da326cc70be20e05823e42ef6152c216
Orionchik182 <banjik13@gmail.com>
2026-04-18 18:54:10 +0400

    refactor: remove database exit process and delete vercel configuration file

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -7,6 +7,5 @@ export const connectDB = async () => {
     console.log(`Database connected successfully`);
   } catch (error) {
     console.error("Database connection failed", error.message);
-    process.exit(1); // exit code 1 means failure, 0 means success
   }
 };
