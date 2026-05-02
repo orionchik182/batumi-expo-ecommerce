74a75461fd108b059e02246d9e4bed1b293b4193
Orionchik182 <banjik13@gmail.com>
2026-05-02 16:23:51 +0400

    feat: implement connection pooling check in connectDB to prevent redundant database connections

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,9 +1,17 @@
 import mongoose from "mongoose";
 import { ENV } from "./env.js";
 
+let isConnected = false;
+
 export const connectDB = async () => {
+  if (isConnected) {
+    console.log("Using existing database connection");
+    return;
+  }
+  
   try {
     const conn = await mongoose.connect(ENV.DB_URL);
+    isConnected = !!conn.connections[0].readyState;
     console.log(`Database connected successfully`);
   } catch (error) {
     console.error("Database connection failed", error.message);
